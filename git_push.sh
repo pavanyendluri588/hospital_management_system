dd3b79b1e624cdff35114b18b1604d29450fae83
pavanyendluri588 <pavanyendluri588@gmail.com>
2022-06-15 12:48:42 +0530

    april_2nd_upload

--- git_push.sh
+++ git_push.sh
@@ -1,6 +1,7 @@
 git init
 git remote add origin https://github.com/pavanyendluri588/hospital_management_system.git
 git remote -v
-git add  . --force
-git commit -m "first commit"
-git push -u origin main  --force
+git add . --force
+git commit -m "april_2nd_upload" 
+git push origin main --force
+
