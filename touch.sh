44d4e8b009d68809a71f4752fd76056eb8e6e76a
pavanyendluri588 <pavanyendluri588@gmail.com>
2022-07-17 01:56:54 +0530

    april_2nd_upload

---
+++ touch.sh
@@ -0,0 +1,10 @@
+git init
+git remote add origin https://github.com/pavanyendluri588/hospital_management_system.git
+git remote -v  --force
+git add . --force
+git commit -m "april_2nd_upload"
+git config --global push.autoSetupRemote true
+git pull --rebase origin main
+
+git push -f origin main
+
