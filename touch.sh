5951a9fde3858cdce3b441dea06f952ccd5b300a
pavanyendluri588 <pavanyendluri588@gmail.com>
2022-07-17 01:57:21 +0530

    april_2nd_upload

--- touch.sh
+++ touch.sh
@@ -4,7 +4,7 @@ git remote -v  --force
 git add . --force
 git commit -m "april_2nd_upload"
 git config --global push.autoSetupRemote true
-git pull --rebase origin main
+git pull --rebase origin master
 
-git push -f origin main
+git push -f origin master
 
